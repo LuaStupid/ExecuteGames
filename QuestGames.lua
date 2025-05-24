975070181ae99436fcc8a29920c8238bb70db80b
Hiru Hub <viplam479@gmail.com>
2025-05-24 09:09:37 +0700

    Update QuestGames.lua

--- QuestGames.lua
+++ QuestGames.lua
@@ -1,22 +1,6 @@
-local Games = {
-    --Blox Fruits
-    [2753915549] = "https://raw.githubusercontent.com/LuaStupid/Games/main/BloxFruits.lua",
-    [4442272183] = "https://raw.githubusercontent.com/LuaStupid/Games/main/BloxFruits.lua",
-    [7449423635] = "https://raw.githubusercontent.com/LuaStupid/Games/main/BloxFruits.lua",
-    --MemeSea
-    [10260193230] = "https://raw.githubusercontent.com/LuaStupid/Games/main/MemeSea.lua",
-    --BuildABoatForTreasure
-    [537413528] = "https://raw.githubusercontent.com/LuaStupid/Games/main/Build-A-Boat-For-Treasure.lua",
-    --LegendsOfSpeed
-    [3101667897] = "https://raw.githubusercontent.com/LuaStupid/Games/main/LegendsOfSpeed.lua",
-    --NinjaLegends
-    [3956818381] = "https://raw.githubusercontent.com/LuaStupid/Games/main/NinjaLegends.lua"
-}
-local placeId = game.PlaceId
-if Games[placeId] then
-    if placeId == 2753915549 or placeId == 4442272183 or placeId == 7449423635 then
-        getgenv().Team = "Pirates"
-        getgenv().Chat = "Hiru Hub On Top"
+local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaStupid/Games/main/Games.lua"))()
+for PlaceID, Execute in pairs(Games) do
+    if PlaceID == game.PlaceId then
+        loadstring(game:HttpGet(Execute))()
     end
-    loadstring(game:HttpGet(Games[placeId]))()
 end
